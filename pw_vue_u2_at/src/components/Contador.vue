3403221d766fa1fea53b66eac1e9aa956b8df4f9
AlexisETB <alexistroya22@gmail.com>
2025-11-21 19:42:50 -0500

    feat: add Contador and ContadorView components, update App.vue structure, and configure dev server port

---
+++ pw_vue_u2_at/src/components/Contador.vue
@@ -0,0 +1,18 @@
+<!--  html del componente -->
+ <template>
+  <div>
+    <h1>Universidad Central del Ecuador</h1>
+    <h2>Carrera de Ingeniería de Computación</h2>
+  </div>
+</template>
+
+<script>
+// lógica del componente
+export default {
+  // declaracion de option api
+};
+</script>
+
+<style>
+/* estilos del css del componente */
+</style>
